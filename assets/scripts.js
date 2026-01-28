27be6b7087144c1055c4ce67202fb6e3a4d0e918
RS-1900 <leonfushi2003@gmail.com>
2026-01-28 08:22:12 -0500

    first commit

---
+++ assets/scripts.js
@@ -0,0 +1,10 @@
+class streaming {
+    constructor (titulo, descripcion, miniatura, tipo){
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.miniatura = miniatura;
+        this.tipo = tipo;
+        this.capitulos = [];
+    }
+    
+}
