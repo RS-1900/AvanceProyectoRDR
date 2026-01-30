70da50645cb4216987ab50d98f91004d3326f858
RS-1900 <leonfushi2003@gmail.com>
2026-01-30 00:03:07 -0500

    inicio de sesion, crear usuario, cerrar sesion
    + css + js

--- scripts.js
+++ scripts.js
@@ -37,6 +37,92 @@ class gestorDeContenido {
         this.contenidos.push(content);
     }
 }
+
+/************ */
+
+// para registrarse
+const registroForm = document.getElementById("registroForm");
+
+if (registroForm) {
+    registroForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const username = document.querySelector(".username").value;
+        const password = document.querySelector(".password").value;
+
+        // Obtener usuarios guardados o array vacío
+        let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+        // Verificar si el usuario ya existe
+        const existe = usuarios.some(user => user.username === username);
+
+        if (existe) {
+            alert("El usuario ya existe");
+            return;
+        }
+
+        // Guardar nuevo usuario
+        usuarios.push({ username, password });
+        localStorage.setItem("usuarios", JSON.stringify(usuarios));
+
+        alert("Usuario creado correctamente");
+
+        // Redirigir a pagos.html despues de crear cuenta
+        window.location.href = "pagos.html";
+    });
+}
+
+// iniciar sesion
+const loginForm = document.getElementById("loginForm");
+
+if (loginForm) {
+    loginForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const username = document.querySelector(".username").value;
+        const password = document.querySelector(".password").value;
+
+        let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+        // Buscar usuario
+        const usuarioValido = usuarios.find(
+            user => user.username === username && user.password === password
+        );
+
+        if (usuarioValido) {
+            alert("Inicio de sesión correcto");
+            localStorage.setItem("usuarioActivo", usuarioValido.username)
+            window.location.href = "paginaprincipal.html";
+        } else {
+            alert("Usuario o contraseña incorrectos");
+        }
+    });
+}
+
+
+//pa cerrar la sesion
+const logoutBtn = document.querySelector(".logout-btn");
+
+if (logoutBtn) {
+    logoutBtn.addEventListener("click", () => {
+        // Borra la sesión activa
+        localStorage.removeItem("usuarioActivo");
+
+        // Redirige al login
+        window.location.href = "index.html";
+    });
+}
+
+
+function myFunction() {
+    window.location.href = "paginaprincipal.html";
+}
+
+
+
+
+
+
 //Funciones de overlay
 function mostrarDetalle(index){
     const contenido = gestor.contenidos[index];
@@ -92,3 +178,6 @@ gestor.contenidos.forEach((elemento,index) => {
 
     contenedor.appendChild(tarjeta);
 })
+
+
+
